4728cfd6be910a1ed8e576fd25c8cb3393231af2
Thanh Nguyen <thanhnguyenttn@gmail.com>
2021-02-04 11:45:05 -0500

    AWS currently being set up in backend

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { ValidationError } = require('sequelize');
+const bodyParser = require("body-parser");
 
 const routes = require('./routes');
 const { environment } = require('./config');
@@ -15,7 +16,8 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Security Middleware
 if (!isProduction) {
