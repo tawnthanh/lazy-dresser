aaba2c135896633033c31af564672adcba2b8463
Thanh Nguyen <thanhnguyenttn@gmail.com>
2021-02-08 13:00:45 -0500

    Added backend heroku security

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,14 @@ const isProduction = environment === 'production';
 
 const app = express();
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 app.use(morgan('dev'));
 
 app.use(cookieParser());
