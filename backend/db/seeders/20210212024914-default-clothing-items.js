0599efba50c0935bd25a6c7063d0840a6f8ec22d
Thanh Nguyen <thanhnguyenttn@gmail.com>
2021-02-11 22:55:05 -0500

    working on some seeder but bug with imgs

---
+++ backend/db/seeders/20210212024914-default-clothing-items.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('ClothingItems', [
+      // title, description, imgUrl, primaryColor, secondaryColor, itemTypeId, fitId, userId, occasionId
+      {title:"boyfriend jeans",description: "they're alright", imgUrl: "../../public/imgs/b-1.jpg", primaryColor:"#4b639c", secondaryColor: "#a4bcec", itemTypeId: 5, fitId: 2, userId: 1, occasionId: 3},
+      {title:"leather leggings",description: null, imgUrl: "../../public/imgs/b-2.jpg", primaryColor:"#424242", secondaryColor: "#424242", itemTypeId: 5, fitId: 3, userId: 1, occasionId: 2},
+      {title:"skinny jeans",description: "goes with everything", imgUrl: "../../public/imgs/b-3.jpg", primaryColor:"#243755", secondaryColor: "#243755", itemTypeId: 5, fitId: 1, userId: 1, occasionId: 1},
+      {title:"dressy wideleg pants",description: "Idk what to wear with this. Worn once?", imgUrl: "../../public/imgs/b-5.jpg", primaryColor:"#f4c297", secondaryColor: "#f4c297", itemTypeId: 5, fitId: 2, userId: 1, occasionId: 6},
+      {title:"brown plaid skirt",description: "living my dark academia life", imgUrl: "../../public/imgs/b-6.jpg", primaryColor:"#54442c", secondaryColor: "#54442c", itemTypeId: 5, fitId: 1, userId: 1, occasionId: 1},
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
